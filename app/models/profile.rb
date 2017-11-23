a51a94924cea22a67d89f5e66749963d83dfeb78
Sean Luckett <whitecrane007@gmail.com>
2017-11-23 11:19:56 -0700

    Adds #name to Profile for displaying full name

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,4 +2,8 @@ class Profile < ApplicationRecord
   belongs_to :user
 
   enum sexual_id: { who_cares: 0, female: 1, male: 2, neither: 3, both: 4 }
+
+  def name
+    "#{first_name} #{last_name}"
+  end
 end
--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#name' do
+    it 'returns first and last name' do
+      user = build :user
+      profile = user.build_profile first_name: 'Joe', last_name: 'Seephus'
+      expect(profile.name).to eq 'Joe Seephus'
+    end
+  end
 end
