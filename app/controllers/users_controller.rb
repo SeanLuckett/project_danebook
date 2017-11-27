e35bd5f96ab47d2b0b97b1c25cb70e84ae32b3d9
Sean Luckett <whitecrane007@gmail.com>
2017-11-27 14:47:23 -0700

    Adds logout path; fixes a few signup bugs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         sign_in @user
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.html { redirect_to user_profile_path(@user, @user.profile), notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
