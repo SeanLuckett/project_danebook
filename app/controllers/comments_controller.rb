76de6867998dcf16c275f03b7da6fb84befe59a7
Sean Luckett <whitecrane007@gmail.com>
2017-12-07 08:21:16 -0700

    Adds ability to create/delete comments on posts

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,26 @@
+class CommentsController < ApplicationController
+
+  def create
+    post = Post.find params[:post_id]
+    post.comments.build comment_params.merge(user_id: current_user.id)
+
+    respond_to do |format|
+      msg = post.save ? 'Comment commented, or commented comment, if you will!' :'Could not save your comment.'
+      format.html { redirect_to timeline_path(post.user), notice: msg }
+    end
+  end
+
+  def destroy
+    comment = Comment.find(params[:id])
+    poster = comment.post.user
+
+    msg = comment.destroy ? 'Comment deleted' : 'Failed to delete comment'
+    redirect_to timeline_path(poster), notice: msg
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit :body
+  end
+end
